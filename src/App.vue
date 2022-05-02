2f54877b19e08800aec3cdf61469a514bc21971a
leticia <leticiaborgess.oficial@gmail.com>
2022-05-02 11:08:27 -0300

    Enviar

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script>
+import BlocoCampus from "@/components/BlocoCampus.vue";
+import BlocoA from "@/components/BlocoA.vue";
+import BlocoB from "@/components/BlocoB.vue";
+import BlocoC from "@/components/BlocoC.vue";
+import BlocoD from "@/components/BlocoD.vue";
+import BlocoE from "@/components/BlocoE.vue";
+import BlocoEncubatório from "@/components/BlocoEncubatório.vue";
+import BlocoGinasio from "@/components/BlocoGinasio.vue";
+import BlocoFazenda from "@/components/BlocoFazenda.vue";
+
+
+export default {
+  components: {
+    BlocoCampus,
+    BlocoA,
+    BlocoB,
+    BlocoC,
+    BlocoD,
+    BlocoE,
+    BlocoEncubatório,
+    BlocoGinasio,
+    BlocoFazenda
+  },
+  data() {
+    return {
+      atual: "campus",
+    };
+  },
+  methods: {
+    alteraAtual(valor) {
+      this.atual = valor;
+    },
+  },
+};
+</script>
+
+<template>
+  {{ atual }}
+  <BlocoCampus
+    v-if="atual === 'BlocoCampus'"
+    @altera-atual="alteraAtual"
+  ></BlocoCampus>
+  <BlocoA v-if="atual === 'BlocoA'" @altera-atual="alteraAtual"></BlocoA>
+  <BlocoB v-if="atual === 'BlocoB'" @altera-atual="alteraAtual"></BlocoB>
+  <BlocoC v-if="atual === 'BlocoC'" @altera-atual="alteraAtual"></BlocoC>
+  <BlocoD v-if="atual === 'BlocoD'" @altera-atual="alteraAtual"></BlocoD>
+  <BlocoE v-if="atual === 'BlocoE'" @altera-atual="alteraAtual"></BlocoE>
+  <BlocoEncubatório
+    v-if="atual === 'BlocoEncubatório'"
+    @altera-atual="alteraAtual"
+  ></BlocoEncubatório>
+  <BlocoGinasio
+    v-if="atual === 'BlocoGinasio'"
+    @altera-atual="alteraAtual"
+  ></BlocoGinasio>
+  <BlocoFazenda v-if="atual === 'BlocoFazenda'" @altera-atual="alteraAtual"><BlocoFazenda>
+</template>
+
+<style></style>
---
+++ src/components/BlocoC.vue
@@ -0,0 +1,20 @@
+<template>
+  <input id="menu-projeto" type="checkbox" />
+  <label for="menu-projeto">
+    <div class="menu">
+      <span class="projeto"></span>
+    </div>
+  </label>
+
+  <ul>
+    <li @click="$emit('altera-atual', 'campus')">Início</li>
+    <li @click="$emit('altera-atual', 'campus')">Bloco A</li>
+    <li @click="$emit('altera-atual', 'campus')">Bloco B</li>
+    <li @click="$emit('altera-atual', 'campus')">Bloco C</li>
+    <li @click="$emit('altera-atual', 'campus')">Bloco D</li>
+    <li @click="$emit('altera-atual', 'campus')">Bloco E</li>
+    <li @click="$emit('altera-atual', 'campus')">Ginásio</li>
+    <li @click="$emit('altera-atual', 'campus')">Encubatório</li>
+    <li @click="$emit('altera-atual', 'campus')">Fazenda</li>
+  </ul>
+</template>
